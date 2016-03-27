d16710556e7408a80459e8ea838b78888ec1d797
Mendelt Siebenga <msiebenga@gmail.com>
2016-03-27 18:51:36 +0200

    Get and set the agentx socket

--- agentpy/agentpy.py
+++ agentpy/agentpy.py
@@ -1,11 +1,34 @@
-import ctypes
+from ctypes import cdll, c_char_p, c_int
 import ctypes.util
 
 
 class Agent(object):
 
+    DS_LIBRARY_ID = 0
+    DS_APPLICATION_ID = 1
+    DS_TOKEN_ID = 2
+
+    DS_AGENT_X_SOCKET = 1
+
     def __init__(self):
-        self._netsnmpagent = ctypes.cdll.LoadLibrary(ctypes.util.find_library('netsnmpagent'))
+        self._netsnmpagent = self._init_library()
+
+    def _init_library(self):
+        netsnmpagent = cdll.LoadLibrary(ctypes.util.find_library('netsnmpagent'))
+
+        netsnmpagent.netsnmp_ds_set_string.argtypes = [c_int, c_int, c_char_p]
+        netsnmpagent.netsnmp_ds_set_string.restype = c_int
+
+        netsnmpagent.netsnmp_ds_get_string.argtypes = [c_int, c_int]
+        netsnmpagent.netsnmp_ds_get_string.restype = c_char_p
+
+        return netsnmpagent
 
     def init_agent(self, agent_name):
         self._netsnmpagent.init_agent(agent_name)
+
+    def ds_set_string(self, val1, val2, val3):
+        self._netsnmpagent.netsnmp_ds_set_string(val1, val2, val3)
+
+    def ds_get_string(self, val1, val2):
+        return self._netsnmpagent.netsnmp_ds_get_string(val1, val2)
--- agentpy/main.py
+++ agentpy/main.py
@@ -6,5 +6,9 @@ def main():
     test = agent.init_agent('Stuff')
     print(test)
 
+    agent.ds_set_string(agentpy.Agent.DS_APPLICATION_ID, agentpy.Agent.DS_AGENT_X_SOCKET, 'agentx_socket')
+    test = agent.ds_get_string(agentpy.Agent.DS_APPLICATION_ID, agentpy.Agent.DS_AGENT_X_SOCKET)
+    print(test)
+
 if __name__ == '__main__':
     main()
