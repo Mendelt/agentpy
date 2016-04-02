e15241537d6050868db421aed540dedbb6be3f98
Mendelt Siebenga <msiebenga@gmail.com>
2016-04-02 22:28:13 +0200

    Moved netsnmpagent bindings to separate class.

--- agentpy/agentpy.py
+++ agentpy/agentpy.py
@@ -3,31 +3,67 @@ import ctypes.util
 
 
 class Agent(object):
+    def __init__(self, agent_name, mib_path, socket):
+        self._mib_path = mib_path
+        self._started = False
+        self._lib = NetSNMPWrapper()
 
-    DS_LIBRARY_ID = 0
-    DS_APPLICATION_ID = 1
-    DS_TOKEN_ID = 2
+        # Set the master socket
+        self._lib.ds_set_string(DS_APPLICATION_ID, DS_AGENT_X_SOCKET, socket)
 
-    # Boolean values
-    DS_AGENT_VERBOSE = 0  # 1 if verbose output desired
-    DS_AGENT_ROLE = 1  # 0 if master, 1 if client
-    DS_AGENT_NO_ROOT_ACCESS = 2  # 1 if we can't get root access
-    DS_AGENT_AGENTX_MASTER = 3  # 1 if AgentX desired
-    DS_AGENT_QUIT_IMMEDIATELY = 4  # 1 to never start the agent
-    DS_AGENT_DISABLE_PERL = 5  # 1 to never enable perl
-    DS_AGENT_NO_CONNECTION_WARNINGS = 6  # 1 = !see !connect msgs
-    DS_AGENT_LEAVE_PIDFILE = 7  # 1 = leave PID file on exit
-    DS_AGENT_NO_CACHING = 8  # 1 = disable netsnmp_cache
-    DS_AGENT_STRICT_DISMAN = 9  # 1 = "correct" object ordering
-    DS_AGENT_DONT_RETAIN_NOTIFICATIONS = 10  # 1 = disable trap logging
-    DS_AGENT_DONT_LOG_TCPWRAPPERS_CONNECTS = 12  # 1 = disable logging
-    # TODO DS_APP_DONT_LOG         NETSNMP_DS_AGENT_DONT_RETAIN_NOTIFICATIONS /* compat */
-    DS_AGENT_SKIPNFSINHOSTRESOURCES = 13  # 1 = don't store NFS entries in hrStorageTable
-    DS_AGENT_REALSTORAGEUNITS = 14  # 1 = use real allocation units in hrStorageTable, 0 = recalculate it to fit 32bits
+        # Set the agent role
+        self._lib.ds_set_boolean(DS_APPLICATION_ID, DS_AGENT_ROLE, True)
 
-    # String values
-    DS_AGENT_X_SOCKET = 1
+        # Init
+        self._lib.init_agent(agent_name)
+        self._lib.init_snmp(agent_name)
 
+        # Load the MIB
+        self._lib.init_mib()
+        self._lib.read_mib('mibs/AGENTPY-TESTING-MIB.txt')
+
+    def run(self):
+        self._started = True
+
+        while self._started:
+            self._lib.agent_check_and_process(1)
+
+        self._lib.snmp_shutdown()
+
+    def stop(self):
+        self._started = False
+
+    @property
+    def socket(self):
+        return self._lib.ds_get_string(DS_APPLICATION_ID, DS_AGENT_X_SOCKET)
+
+
+DS_LIBRARY_ID = 0
+DS_APPLICATION_ID = 1
+DS_TOKEN_ID = 2
+
+# Boolean values
+DS_AGENT_VERBOSE = 0  # 1 if verbose output desired
+DS_AGENT_ROLE = 1  # 0 if master, 1 if client
+DS_AGENT_NO_ROOT_ACCESS = 2  # 1 if we can't get root access
+DS_AGENT_AGENTX_MASTER = 3  # 1 if AgentX desired
+DS_AGENT_QUIT_IMMEDIATELY = 4  # 1 to never start the agent
+DS_AGENT_DISABLE_PERL = 5  # 1 to never enable perl
+DS_AGENT_NO_CONNECTION_WARNINGS = 6  # 1 = !see !connect msgs
+DS_AGENT_LEAVE_PIDFILE = 7  # 1 = leave PID file on exit
+DS_AGENT_NO_CACHING = 8  # 1 = disable netsnmp_cache
+DS_AGENT_STRICT_DISMAN = 9  # 1 = "correct" object ordering
+DS_AGENT_DONT_RETAIN_NOTIFICATIONS = 10  # 1 = disable trap logging
+DS_AGENT_DONT_LOG_TCPWRAPPERS_CONNECTS = 12  # 1 = disable logging
+# TODO DS_APP_DONT_LOG         NETSNMP_DS_AGENT_DONT_RETAIN_NOTIFICATIONS /* compat */
+DS_AGENT_SKIPNFSINHOSTRESOURCES = 13  # 1 = don't store NFS entries in hrStorageTable
+DS_AGENT_REALSTORAGEUNITS = 14  # 1 = use real allocation units in hrStorageTable, 0 = recalculate it to fit 32bits
+
+# String values
+DS_AGENT_X_SOCKET = 1
+
+
+class NetSNMPWrapper(object):
     def __init__(self):
         self._netsnmpagent = self._init_library()
 
--- agentpy/main.py
+++ agentpy/main.py
@@ -1,28 +1,31 @@
 import agentpy
-from agentpy import Agent
 
 
 def main():
-    agent = Agent()
+    # agent = agentpy.LibNetSNMP()
+    #
+    # agent.ds_set_string(agentpy.DS_APPLICATION_ID, agentpy.DS_AGENT_X_SOCKET, 'agentx_socket')
+    # print(agent.ds_get_string(agentpy.DS_APPLICATION_ID, agentpy.DS_AGENT_X_SOCKET))
+    #
+    # print(agent.ds_get_boolean(agentpy.DS_APPLICATION_ID, agentpy.DS_AGENT_ROLE))
+    # agent.ds_set_boolean(agentpy.DS_APPLICATION_ID, agentpy.DS_AGENT_ROLE, True)
+    # print(agent.ds_get_boolean(agentpy.DS_APPLICATION_ID, agentpy.DS_AGENT_ROLE))
+    # agent.ds_toggle_boolean(agentpy.DS_APPLICATION_ID, agentpy.DS_AGENT_ROLE)
+    # print(agent.ds_get_boolean(agentpy.DS_APPLICATION_ID, agentpy.DS_AGENT_ROLE))
+    # agent.ds_toggle_boolean(agentpy.DS_APPLICATION_ID, agentpy.DS_AGENT_ROLE)
+    #
+    # agent.init_agent('Stuff')
+    # agent.init_snmp('Stuff')
+    #
+    # agent.init_mib()
+    # agent.read_mib('mibs/AGENTPY-TESTING-MIB.txt')
+    #
+    # agent.agent_check_and_process(0)
+    # agent.snmp_shutdown('Stuff')
 
-    agent.ds_set_string(agentpy.Agent.DS_APPLICATION_ID, agentpy.Agent.DS_AGENT_X_SOCKET, 'agentx_socket')
-    print(agent.ds_get_string(agentpy.Agent.DS_APPLICATION_ID, agentpy.Agent.DS_AGENT_X_SOCKET))
+    agent = agentpy.Agent('Agent', 'mibs/AGENTPY-TESTING-MIB.txt', 'socket')
+    agent.run()
 
-    print(agent.ds_get_boolean(agentpy.Agent.DS_APPLICATION_ID, agentpy.Agent.DS_AGENT_ROLE))
-    agent.ds_set_boolean(agentpy.Agent.DS_APPLICATION_ID, agentpy.Agent.DS_AGENT_ROLE, True)
-    print(agent.ds_get_boolean(agentpy.Agent.DS_APPLICATION_ID, agentpy.Agent.DS_AGENT_ROLE))
-    agent.ds_toggle_boolean(agentpy.Agent.DS_APPLICATION_ID, agentpy.Agent.DS_AGENT_ROLE)
-    print(agent.ds_get_boolean(agentpy.Agent.DS_APPLICATION_ID, agentpy.Agent.DS_AGENT_ROLE))
-    agent.ds_toggle_boolean(agentpy.Agent.DS_APPLICATION_ID, agentpy.Agent.DS_AGENT_ROLE)
-
-    agent.init_agent('Stuff')
-    agent.init_snmp('Stuff')
-
-    agent.init_mib()
-    agent.read_mib('mibs/AGENTPY-TESTING-MIB.txt')
-
-    agent.agent_check_and_process(0)
-    agent.snmp_shutdown('Stuff')
 
 if __name__ == '__main__':
     main()
